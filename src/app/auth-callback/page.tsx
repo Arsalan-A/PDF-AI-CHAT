daf0689ed3b5ee7bf4d7fbcdb750e9c696f07a5c
Arsalan <17147640+Arsalan-A@users.noreply.github.com>
2024-04-11 02:35:54 -0400

    UI updates and db integration

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
+import { useEffect } from 'react';
+
+const Page = () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+
+  const origin = searchParams.get('origin');
+
+  const { data, isLoading, isError, error } = trpc.authCallback.useQuery(
+    undefined,
+    {
+      retry: true,
+      retryDelay: 500,
+    }
+  );
+
+  useEffect(() => {
+    if (data?.success) {
+      router.push(origin ? `/${origin}` : '/dashboard');
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.success, origin]);
+
+  useEffect(() => {
+    if (isError && error.data?.code === 'UNAUTHORIZED') {
+      router.push('/sign-in');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isError, error]);
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>Setting up tour account...</h3>
+        <p>You will be redirected automatically</p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
